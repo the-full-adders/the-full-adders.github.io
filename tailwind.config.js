65d1aa322d83ee624e33913e7fbca8de9166036a
p0lygun <solankivibhakar82@gmail.com>
2023-08-30 18:50:00 +0530

    feat: add member info

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{html,js}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+          'montserrat': ['Montserrat'],
+      }
+    },
   },
   plugins: [],
 }
